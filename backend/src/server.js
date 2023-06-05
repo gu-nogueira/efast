4d6535f0fdceec65f8a88baf0a045e0b2282994a
Gustavo Nogueira <gus.h.nogueira@gmail.com>
2023-06-05 08:34:47 -0300

    chore(backend) removes useless app_url env

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ console.log('Loading app...');
 
 import app from './app';
 
-app.listen(process.env.APP_PORT || 2000);
-console.log(`Server is listening in: ${process.env.APP_URL}`);
+app.listen(process.env.APP_PORT || 80);
+console.log(`Server is listening in port ${process.env.APP_PORT || 80}!`);
